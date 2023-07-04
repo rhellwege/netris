a2855bd3105abce2b3f64c3ea7223edf233225f8
Ryan Hellwege <rhellwege6@gmail.com>
2023-07-04 15:24:45 -0700

    added sounds

--- src/netris.nim
+++ src/netris.nim
@@ -1,6 +1,18 @@
 import raylib
-import std/random,strformat
+import std/random, strformat
+import system
 include "settings.nims"
+
+const songStr = staticRead("../assets/Tetris.wav")
+const hitSoundStr = staticRead("../assets/blip.wav")
+const clearSoundStr = staticRead("../assets/bitbounce.wav")
+const gameOverStr = staticRead("../assets/gameover.wav")
+
+initAudioDevice() # TODO: REFACTOR SOUNDS
+let song = loadSoundFromWave(loadWaveFromMemory(".wav", songStr.toOpenArrayByte(0, len(songStr)-1)))
+let hitSound = loadSoundFromWave(loadWaveFromMemory(".wav", hitSoundStr.toOpenArrayByte(0, len(hitSoundStr)-1)))
+let clearSound = loadSoundFromWave(loadWaveFromMemory(".wav", clearSoundStr.toOpenArrayByte(0, len(clearSoundStr)-1)))
+let gameOverSound = loadSoundFromWave(loadWaveFromMemory(".wav", gameOverStr.toOpenArrayByte(0, len(gameOverStr)-1)))
 
 type
   GameState = object
@@ -15,17 +27,18 @@ type
     posY: int
     rotation: Rotation
     board: array[boardRows, array[boardCols, TetrominoType]]
-# ----------------------------------------------------------------------------------------
+
 proc inBounds(g: GameState): bool =
   for i in countUp(0, 3):
-    for j in countUp(0,3):
+    for j in countUp(0, 3):
       if tetrominos[ord g.activeTetromino][ord g.rotation][i][j]:
-        if (g.posX + j) < 0 or g.posY + i < 0 or g.posX + j >= boardCols or g.posY + i >= boardRows:
+        if (g.posX + j) < 0 or g.posY + i < 0 or g.posX + j >= boardCols or
+            g.posY + i >= boardRows:
           return false
         if g.board[i + g.posY][j + g.posX] != None:
           return false
   return true
-  
+
 
 proc attemptMove(g: var GameState, dx: int, dy: int): bool =
   let curX = g.posX
@@ -52,26 +65,28 @@ proc collides(g: GameState): bool =
   for i in countDown(3, 0):
     for j in countUp(0, 3):
       if tetrominos[ord g.activeTetromino][ord g.rotation][i][j]:
-        if g.posY + i >= boardRows - 1 or g.board[g.posY + i + 1][g.posX + j] != None: return true
+        if g.posY + i >= boardRows - 1 or g.board[g.posY + i + 1][g.posX + j] !=
+            None: return true
   return false
 
 proc clearFilledRows(g: var GameState) =
-  var 
+  var
     clearedRows = 0
     startRow = g.posY + 3
     endRow: int
   echo "checking 4 rows starting from: ", startRow
   for row in countDown(startRow, g.posY):
     var clear = true
-    for col in countUp(0,boardCols-1):
-      if g.board[row][col] == None: 
+    for col in countUp(0, boardCols-1):
+      if g.board[row][col] == None:
         clear = false
         break
     if clear:
       if clearedRows == 0: startRow = row
       inc clearedRows
-  
+
   if clearedRows == 0: return
+  playSound(clearSound)
 
   endRow = startRow - clearedRows + 1
   echo "Clearing rows ", startRow, " - ", endRow
@@ -82,23 +97,26 @@ proc clearFilledRows(g: var GameState) =
     for col in countUp(0, boardCols-1):
       g.board[swapRow][col] = g.board[row][col]
       g.board[row][col] = None
-  
+
   echo "Successfully cleared ", clearedRows, " rows"
   g.score += clearedRows
   dec g.speed
 
-# ----------------------------------------------------------------------------------------
-
 proc main =
   initWindow(screenWidthPx, screenHeightPx, windowTitle)
+  
+  defer: closeAudioDevice()
   setTargetFPS(fps)
   setExitKey(Delete)
   randomize()
+  
+  playSound(song)
+  setSoundVolume(song, 0.3)
 
   var game = GameState(
     posX: int boardCols/2 - 2, posY: 0,
-    activeTetromino: TetrominoType rand(6) + 1, 
-    nextTetromino: TetrominoType rand(6) + 1, 
+    activeTetromino: TetrominoType rand(6) + 1,
+    nextTetromino: TetrominoType rand(6) + 1,
     speed: initialSpeed, frames: 0, lastBreath: initialLastBreath,
     rotation: 0, paused: false, score: 0)
 
@@ -107,21 +125,24 @@ proc main =
   echo game.activeTetromino
 
   while not windowShouldClose():
+    
     # Update
-    # ------------------------------------------------------------------------------------
     # handle input:
     if isKeyPressed(Escape): game.paused = not game.paused
+
     if not game.paused:
-      if isKeyPressed(Up): 
-        if game.attemptRotation(): 
+      if not isSoundPlaying(song):
+        playSound(song) # replay the song
+      if isKeyPressed(Up):
+        if game.attemptRotation():
           movedAround = true
-      if isKeyDown(Left): 
+      if isKeyDown(Left):
         if game.attemptMove(-1, 0):
           movedAround = true
-      if isKeyDown(Right): 
+      if isKeyDown(Right):
         if game.attemptMove(1, 0):
           movedAround = true
-      if isKeyDown(Down): 
+      if isKeyDown(Down):
         discard game.attemptMove(0, 1)
         movedDown = true
         movedAround = true
@@ -130,17 +151,17 @@ proc main =
           inc game.posY
         movedDown = true
         movedAround = true
-      
       if isKeyDown(Space) or isKeyDown(Up):
         movedAround = true
-
       # handle collision
       if game.collides():
         if not movedAround or game.lastBreath == 0:
+          playSound(hitSound)
           for i in countUp(0, 3):
             for j in countUp(0, 3):
               if tetrominos[ord game.activeTetromino][ord game.rotation][i][j]:
                 game.board[game.posY + i][game.posX + j] = game.activeTetromino
+
           game.clearFilledRows()
           # reset game state for new active tetromino
           game.rotation = 0
@@ -149,21 +170,22 @@ proc main =
           game.posX = int boardCols/2 - 2
           game.posY = 0
           game.lastBreath = initialLastBreath
-          if not game.inBounds(): 
-            echo "you lost!"
+          if not game.inBounds():
+            stopSound(song)
+            playSound(gameoverSound)
             game.paused = true # you just lost
         else:
           dec game.lastBreath
       else:
         # move tetromino down every few frames
-        if game.frames mod game.speed == 0 and not movedDown: 
+        if game.frames mod game.speed == 0 and not movedDown:
           inc game.posY
-          
+
       inc game.frames
       movedDown = false
       movedAround = false
+
     # Draw
-    # ------------------------------------------------------------------------------------
     beginDrawing()
     clearBackground(RayWhite)
     if showFps:
@@ -171,23 +193,33 @@ proc main =
     if not game.paused:
       # draw the board
       for i in countUp(0, 19):
-        for j in countUp(0,9):
-          drawRectangle(int32 boardRect.x.int32 + (j*cellWidthPx.int32), int32 boardRect.y.int32 + (i*cellWidthPx.int32), cellWidthPx.int32, cellHeightPx.int32, colorSettings[game.board[i][j]])
+        for j in countUp(0, 9):
+          drawRectangle(int32 boardRect.x.int32 + (j*cellWidthPx.int32),
+                        int32 boardRect.y.int32 + (i*cellWidthPx.int32),
+                        cellWidthPx.int32, cellHeightPx.int32,
+                        colorSettings[game.board[i][j]])
       # draw the active tetromino
       for i in countUp(0, 3):
         for j in countUp(0, 3):
           if tetrominos[ord game.activeTetromino][ord game.rotation][i][j]:
-            drawRectangle(int32 boardRect.x.int32 + ((game.posX+j)*cellWidthPx.int32), int32 boardRect.y.int32 + ((game.posY+i)*cellWidthPx.int32), cellWidthPx.int32, cellHeightPx.int32, colorSettings[game.activeTetromino])
-      
+            drawRectangle(int32 boardRect.x.int32 + ((game.posX+j)*cellWidthPx.int32),
+                          int32 boardRect.y.int32 + ((
+                              game.posY+i)*cellWidthPx.int32),
+                          cellWidthPx.int32, cellHeightPx.int32,
+                          colorSettings[game.activeTetromino])
       # draw preview
       for i in countUp(0, 3):
-        for j in  countUp(0, 3):
+        for j in countUp(0, 3):
           if tetrominos[ord game.nextTetromino][0][i][j]:
-            drawRectangle(int32 previewRect.x.int32 + ((1+j)*cellWidthPx.int32), int32 previewRect.y.int32 + ((1+i)*cellWidthPx.int32) - (cellWidthPx/2).int32, cellWidthPx.int32, cellHeightPx.int32, colorSettings[game.nextTetromino])
-      
+            drawRectangle(int32 previewRect.x.int32 + ((1+j)*cellWidthPx.int32),
+                          int32 previewRect.y.int32 + ((
+                              1+i)*cellWidthPx.int32) - (cellWidthPx/2).int32,
+                          cellWidthPx.int32, cellHeightPx.int32,
+                          colorSettings[game.nextTetromino])
+
       # draw score
       drawText(cstring fmt"Score: {game.score}", 30, int32 boardRect.y, 30, Black)
-      
+
       # draw board border
       drawRectangleLines(boardRect, 2.0, BLACK)
 
@@ -195,13 +227,11 @@ proc main =
       drawRectangleLines(previewRect, 2.0, BLACK)
 
     else: # game is paused
-      drawText(cstring "Paused...", int32 screenWidthPx / 2, int32 screenHeightPx / 2, 20, Black)
+      drawText(cstring "Paused...", int32 screenWidthPx / 2,
+          int32 screenHeightPx / 2, 20, Black)
 
     endDrawing()
-    # ------------------------------------------------------------------------------------
 
   closeWindow() # Close window and OpenGL context
-
-# --------------------------------------------------------------------------------------
 
 main()
